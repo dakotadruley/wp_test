267bc97a237bfca0c6fb3338d1a894a92efab941
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-10-24 07:53:39 -0700

    10_24

---
+++ Local Sites/dakotadancing/app/public/wp-content/themes/mock-dance-theme/functions.php
@@ -0,0 +1,15 @@
+<?php 
+
+function dancing_files() {
+    wp_enqueue_style('font_awesome', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('dancing_main_styles', get_stylesheet_uri());
+}
+
+function dance_features() {
+    add_theme_support('title-tag');
+}
+
+add_action('wp_enqueue_scripts', 'dancing_files');
+add_action('after_setup_theme', 'dance_features');
+
+?>
---
+++ Local Sites/dakotadancing/app/public/wp-content/themes/mock-dance-theme/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+    <!-- character settings --> 
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <!-- ALLOWS FOR NATIVE DEVICE SIZE TO BE USED / but CSS needs to be optimized too-->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- this gives wordpress control over the head section -->
+        <?php wp_head(); ?>
+    </head>
+
+<body <?php body_class();  ?>>
+    <header class="site-header">
+    <div class="container">
+      <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>dakota</strong> dancing</a></h1>
+      <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+      <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+      <div class="site-header__menu group">
+        <nav class="main-navigation">
+          <ul>
+            <li class="current-menu-item"><a href="<?php echo site_url('/about-me') ?>">About Me</a></li>
+            <li><a href="#">Programs</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Campuses</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </nav>
+        <div class="site-header__util">
+          <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+          <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
+          <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        </div>
+      </div>
+    </div>
+  </header>
+</body>
+  </html>
+  
+  
+       
