d26c08b9231f7a38e208457e5d9cae3b72eecec5
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-11-18 08:22:19 -0800

    custom query for class dates

--- Local Sites/dakotadancing/app/public/wp-content/themes/mock-dance-theme/front-page.php
+++ Local Sites/dakotadancing/app/public/wp-content/themes/mock-dance-theme/front-page.php
@@ -11,12 +11,15 @@
 <div class="full-width-split group">
 <div class="full-width-split__one">
   <div class="full-width-split__inner">
-    <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+    <h2 class="headline headline--small-plus t-center">Upcoming Classes</h2>
 
 <?php
 $homepageClasses = new WP_Query(array(
-  'posts_per_page' => 2,
-  'post_type' => 'class'
+  'posts_per_page' => -1,
+  'post_type' => 'class',
+  'meta_key' => 'class_date',
+  'orderby' => 'meta_value_num', 
+  'order' => 'ASC'
 ));
 
 while($homepageClasses->have_posts()) {
